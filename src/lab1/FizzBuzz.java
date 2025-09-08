969f38085cf9f2cc6c43c72d397b943a896bd67c
janiceelamm <janicelam0811@gmail.com>
2025-09-08 14:33:40 -0400

    adding the string argument back

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -43,11 +43,11 @@ class FizzBuzz {
         int i = 1;
         while ( i < 100) {
 
-            i = dofizzbuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
